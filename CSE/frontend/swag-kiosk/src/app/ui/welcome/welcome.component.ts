47e89cae91b98a438f48436102a9f738e78accc5
Aweksha <awekshaghimire15@gmail/com>
2025-10-29 22:39:34 -0500

    Initial UI mockup front page

---
+++ CSE/frontend/swag-kiosk/src/app/ui/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card } from "primeng/card";
+
+@Component({
+  selector: 'app-welcome',
+  imports: [Card],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss',
+})
+export class WelcomeComponent {
+
+}
